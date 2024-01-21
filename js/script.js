2c163f9bb6f15c33ed885d4d299abf97de1ae5f2
diw-italy <157099352+diw-italy@users.noreply.github.com>
2024-01-21 16:06:58 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-privacy","displayName":"Privacy","link":{"linkType":"LinkTypePage","href":"#!page-privacy"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-rules","displayName":"Rules","link":{"linkType":"LinkTypePage","href":"#!page-rules"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+    window.silex.data = {"site":{"width":1217},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-privacy","displayName":"Privacy","link":{"linkType":"LinkTypePage","href":"#!page-privacy"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-rules","displayName":"Rules","link":{"linkType":"LinkTypePage","href":"#!page-rules"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+
+function validateForm() {
+    // Validate text fields
+    var fields = ["name", "surname", "institute", "course", "graduationYear", "email", "phone"];
+    for (var i = 0; i < fields.length; i++) {
+        var field = document.forms["RegisterIndustryContest"][fields[i]].value;
+        if (field == null || field == "") {
+            alert("All fields must be filled out");
+            return false;
+        }
+    }
+
+    // Validate gender radio buttons
+    if (!document.querySelector('input[name="gender"]:checked')) {
+        alert("Please select a gender");
+        return false;
+    }
+
+    // Validate checkboxes
+    if (!document.forms["RegisterIndustryContest"]["acceptRegulations"].checked ||
+        !document.forms["RegisterIndustryContest"]["acceptPrivacy"].checked) {
+        alert("You must agree to the contest regulations and privacy policy");
+        return false;
+    }
+
+    // Validate reCAPTCHA
+    var recaptcha = grecaptcha.getResponse();
+    if (recaptcha.length == 0) {
+        alert("Please verify that you are not a robot");
+        return false;
+    }
+
+    return true;
+}
